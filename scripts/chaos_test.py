159918cf7547a016557ac84677db27c8f4bfbc11
Kauan Oliveira <kauandias747474@gmail.com>
2026-05-06 09:55:29 -0300

    Update chaos_test.py

--- scripts/chaos_test.py
+++ scripts/chaos_test.py
@@ -1,1 +1,10 @@
+import time
+import subprocess
 
+def kill_node(name):
+    print(f"Derrubando nó: {name}")
+
+if __name__ == "__main__":
+    kill_node("Node-02")
+    time.sleep(5)
+    print("Verificando se um novo líder foi eleito...")
