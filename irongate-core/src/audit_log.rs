a34d22da8745d8819393485a5eb30ab0c8dca5c8
Kauan Oliveira <kauandias747474@gmail.com>
2026-05-06 08:17:21 -0300

    Update audit_log.rs

--- irongate-core/src/audit_log.rs
+++ irongate-core/src/audit_log.rs
@@ -1,1 +1,8 @@
+pub struct AuditLogger;
 
+impl AuditLogger {
+    pub fn new() -> Self { Self }
+    pub fn log(&self, msg: &str) {
+        println!("[AUDIT - {}]: {}", chrono::Local::now(), msg);
+    }
+}
