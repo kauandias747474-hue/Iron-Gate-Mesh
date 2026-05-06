0d8ee4ee12e8e10751ee9ab7cf0f5467887c69e3
Kauan Oliveira <kauandias747474@gmail.com>
2026-05-06 08:17:52 -0300

    Update ebpf_manager.rs

--- irongate-core/src/ebpf_manager.rs
+++ irongate-core/src/ebpf_manager.rs
@@ -1,1 +1,9 @@
+pub struct EbpfController;
 
+impl EbpfController {
+    pub fn new() -> Self { Self }
+
+    pub fn send_block_command(&self, target_ip: &str) {
+        println!(">>> COMANDO ENVIADO AO DATA PLANE: Bloquear {}", target_ip);
+    }
+}
